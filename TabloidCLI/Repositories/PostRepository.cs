2f9519df4183de4b5df2e3d234ae381fc5566152
Tatiane Komrij <tatiane.b.komrij@gmail.com>
2021-03-19 13:39:43 -0500

    fixing errors in adding post

--- TabloidCLI/Repositories/PostRepository.cs
+++ TabloidCLI/Repositories/PostRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 using TabloidCLI.Models;
 
+
 namespace TabloidCLI.Repositories
 {
     public class PostRepository : DatabaseConnector, IRepository<Post>
@@ -11,7 +12,36 @@ namespace TabloidCLI.Repositories
 
         public List<Post> GetAll()
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id,
+                                               Title,
+                                               Url,
+                                               PublishDateTime
+                                          FROM Post";
+
+                    List<Post> posts = new List<Post>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Post post = new Post()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            PublishDateTime =reader.GetDateTime(reader.GetOrdinal("PublishDateTime"))
+                        };
+                        posts.Add(post);
+                    }
+
+                    reader.Close();
+
+                    return posts;
+                }
+            }
         }
 
         public Post Get(int id)
@@ -79,7 +109,20 @@ namespace TabloidCLI.Repositories
 
         public void Insert(Post post)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Post (Title, Url, PublishDateTime )
+                                                     VALUES (@title, @url, @publishDateTime)";
+                    cmd.Parameters.AddWithValue("@title", post.Title);
+                    cmd.Parameters.AddWithValue("@url", post.Url);
+                    cmd.Parameters.AddWithValue("@publishDateTime", post.PublishDateTime);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Update(Post post)
---
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class PostManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private string _connectionString;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+            Console.WriteLine(" 1) Add Post");
+            Console.WriteLine(" 2) List Posts");
+            Console.WriteLine(" 3) Remove Post");
+            Console.WriteLine(" 4) Edit Post");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Add();
+                    return this;
+               /* case "2":
+                    Author author = Choose();
+                    if (author == null)
+                    {
+                        return this;
+                    }
+                    else
+                    {
+                        return new AuthorDetailManager(this, _connectionString, author.Id);
+                    }
+                case "3":
+                    Add();
+                    return this;
+                case "4":
+                    Edit();
+                    return this;
+                case "5":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;*/
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void Add()
+        {
+            Console.WriteLine("New Post");
+            Post post = new Post();
+
+            Console.Write("Title: ");
+            post.Title = Console.ReadLine();
+
+            Console.Write("URL: ");
+            post.Url = Console.ReadLine();
+
+
+            Console.Write("Publication Date: ");
+            post.PublishDateTime = System.DateTime.Now;
+
+            Console.Write("Choose an author: ");
+            
+            post.Author = Console.ReadLine();
+
+            Console.Write("Blog: ");
+            post.Blog = Console.ReadLine();
+
+            _postRepository.Insert(post);
+        }
+    }
+}
