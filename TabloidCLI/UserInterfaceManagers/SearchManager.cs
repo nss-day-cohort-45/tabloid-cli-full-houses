890c3c3e31a955edfc0995b5b61a0fc030e06a12
Jordan Carter <jordan.carter.0023@gmail.com>
2021-03-22 09:59:35 -0500

    started search

--- TabloidCLI/UserInterfaceManagers/SearchManager.cs
+++ TabloidCLI/UserInterfaceManagers/SearchManager.cs
@@ -29,13 +29,16 @@ namespace TabloidCLI.UserInterfaceManagers
             switch (choice)
             {
                 case "1":
+                    SearchBlogs();
                     return this;
                 case "2":
                     SearchAuthors();
                     return this;
                 case "3":
+                    SearchPosts();
                     return this;
                 case "4":
+                    SearchAll();
                     return this;
                 case "0":
                     return _parentUI;
@@ -59,6 +62,57 @@ namespace TabloidCLI.UserInterfaceManagers
             else
             {
                 results.Display();
+            }
+        }
+        private void SearchBlogs()
+        {
+            Console.Write("Tag> ");
+            string tagName = Console.ReadLine();
+
+            SearchResults<Blog> results = _tagRepository.SearchBlogs(tagName);
+
+            if (results.NoResultsFound)
+            {
+                Console.WriteLine($"No results for {tagName}");
+            }
+            else
+            {
+                results.Display();
+            }
+        }
+        private void SearchPosts()
+        {
+            Console.Write("Tag> ");
+            string tagName = Console.ReadLine();
+
+            SearchResults<Post> results = _tagRepository.SearchPosts(tagName);
+
+            if (results.NoResultsFound)
+            {
+                Console.WriteLine($"No results for {tagName}");
+            }
+            else
+            {
+                results.Display();
+            }
+        }
+        private void SearchAll()
+        {
+            Console.Write("Tag> ");
+            string tagName = Console.ReadLine();
+            
+            SearchResults<Author> aResults = _tagRepository.SearchAuthors(tagName);
+            SearchResults<Blog> bResults = _tagRepository.SearchBlogs(tagName);
+            SearchResults<Post> pResults = _tagRepository.SearchPosts(tagName);
+            if (aResults.NoResultsFound || bResults.NoResultsFound || pResults.NoResultsFound)
+            {
+                Console.WriteLine($"No results for {tagName}");
+            }
+            else
+            {
+                aResults.Display();
+                bResults.Display();
+                pResults.Display();
             }
         }
     }
