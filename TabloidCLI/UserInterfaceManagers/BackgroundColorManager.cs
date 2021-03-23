096f4cbc15cc39769629a60679b961ce0b95816f
Rhowie86 <ronaldnhowie@gmail.com>
2021-03-22 21:11:32 -0500

    added color themes

--- TabloidCLI/UserInterfaceManagers/BackgroundColorManager.cs
+++ TabloidCLI/UserInterfaceManagers/BackgroundColorManager.cs
@@ -18,18 +18,22 @@ namespace TabloidCLI.UserInterfaceManagers
             {
 
                 Console.WriteLine(@"Select a Background Color
-                            1) blue
-                            2) green
-                            3) red
-                            4) Goldenrod?
-                            5) Magenta
-                            6) Gray with Red
-                            7) DarkGreen with White
-                            8) DarkRed with Black
-                            9) White with Black
-                            10) Black with DarkGreen
-                            11) Back to basics
-                            0) Go Back");
+ 1) blue
+ 2) green
+ 3) red
+ 4) Goldenrod?
+ 5) Magenta
+ 6) Gray with Red
+ 7) DarkGreen with White
+ 8) DarkRed with Black
+ 9) White with Black
+ 10) Enter the Matrix?
+ 11) Tatiane Mode
+ 12) Jordan Mode
+ 13) Joshua Mode
+ 14) Back to basics
+ 0) Go Back");
+                Console.Write("Make a theme selection > ");
                 string chosenColor = Console.ReadLine();
 
                 switch (chosenColor)
@@ -85,6 +89,21 @@ namespace TabloidCLI.UserInterfaceManagers
                         Console.Clear();
                         return _parentUI;
                     case "11":
+                        Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Clear();
+                        return _parentUI;
+                    case "12":
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.Clear();
+                        return _parentUI;
+                    case "13":
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Clear();
+                        return _parentUI;
+                    case "14":
                         Console.BackgroundColor = ConsoleColor.Black;
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.Clear();
